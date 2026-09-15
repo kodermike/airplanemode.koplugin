8a6600d81436b1bca540065874c8f8540fa6a92e
Michael Cummings <kodermike@gmail.com>
2026-09-15 10:43:31 -0700

    release bump

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,6 @@ local _ = require("gettext")
 return {
   fullname = _("AirPlaneMode"),
   description = _([[Quickly enable/disable networking and selected plugins in one action.]]),
-  version = "1.9.9",
+  version = "2.0.0",
   release = false,
 }
